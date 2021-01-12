81bb8c3a116c589045455ceef7205a3e2e5c5663
nb26548 <nb26548@novabase.pt>
2021-01-12 16:14:23 +0000

    Adding ViewBinding to Welcome and Instructions Fragments.

--- starter/app/src/main/java/com/udacity/shoestore/InstructionsFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/InstructionsFragment.kt
@@ -1,2 +1,25 @@
 package com.udacity.shoestore
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import com.udacity.shoestore.databinding.FragmentInstructionsBinding
+
+class InstructionsFragment: Fragment() {
+    private lateinit var binding: FragmentInstructionsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentInstructionsBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+}
