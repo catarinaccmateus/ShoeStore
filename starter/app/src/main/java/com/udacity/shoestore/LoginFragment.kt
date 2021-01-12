01aaa053bdc94c27c3c421e4c8ba13c7396fe923
nb26548 <nb26548@novabase.pt>
2021-01-12 15:32:34 +0000

    Developing Welcome Screeen & Adding Instructions Screen

--- starter/app/src/main/java/com/udacity/shoestore/LoginFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/LoginFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.udacity.shoestore.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
@@ -16,7 +17,18 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val binding: FragmentLoginBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+
+        binding.signupButton.setOnClickListener {view: View? ->
+            navigateToWelcomePage(view)
+        }
+        binding.loginButton.setOnClickListener {view: View? ->
+            navigateToWelcomePage(view)
+        }
         return binding.root
+    }
+
+    private fun navigateToWelcomePage(view: View?) {
+        view?.findNavController()?.navigate(R.id.action_loginFragment_to_welcomeFragment)
     }
 
 
