98e081fd11b45812584b6d268172d6fb4d84cc75
nb26548 <nb26548@novabase.pt>
2021-01-13 11:46:54 +0000

    Adding a SharedViewModel to project.

--- starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -2,13 +2,18 @@ package com.udacity.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var viewModel: SharedViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(SharedViewModel::class.java)
+
         setContentView(R.layout.activity_main)
         Timber.plant(Timber.DebugTree())
     }
