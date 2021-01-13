5dfdad4b84a428fd8cca33f49806944fe2205fd2
nb26548 <nb26548@novabase.pt>
2021-01-13 15:16:03 +0000

    Created a Shoe List Fragment and Layout

--- starter/app/src/main/java/com/udacity/shoestore/SharedViewModel.kt
+++ starter/app/src/main/java/com/udacity/shoestore/SharedViewModel.kt
@@ -5,10 +5,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.udacity.shoestore.models.Shoe
 
-val SHOE_ONE = Shoe("Boot 6 INCH PREMIUM", 39.00, "Timberland", "When you think of Timberland boots, youre thinking of these classic waterproof boots.", listOf(""))
-val SHOE_TWO = Shoe("Boot 6 INCH PREMIUM", 36.00, "Timberland", "When you think of Timberland boots, youre thinking of these classic waterproof boots.", listOf(""))
-val SHOE_THREE = Shoe("Boot 6 INCH PREMIUM", 41.00, "Timberland", "When you think of Timberland boots, youre thinking of these classic waterproof boots.", listOf(""))
-val SHOE_FOUR = Shoe("Boot 6 INCH PREMIUM", 45.00, "Timberland", "When you think of Timberland boots, youre thinking of these classic waterproof boots.", listOf(""))
+val SHOE_ONE = Shoe("Boot 6 INCH PREMIUM", 39.00, "Timberland", "When you think of Timberland boots, youre thinking of these classic waterproof boots.", listOf("ic_baseline_shoe_default_24"))
+val SHOE_TWO = Shoe("Cloud X", 36.00, "One", "The On Running Cloud X training shoes mixes training and running into one light and comfortable shoe to help you achieve your lofty goals.", listOf("ic_baseline_shoe_default_24"))
+val SHOE_THREE = Shoe("Pegasus Trail 2", 41.00, "Nike", "Don't let elevation changes, rocks, and loose gravel impede your journey across some serious terrain with the Nike® Pegasus Trail 2 shoes.", listOf("ic_baseline_shoe_default_24"))
+val SHOE_FOUR = Shoe("Solar Boost 21", 45.00, "Adidas", "Synthetic outsole. Textile lining and insole.", listOf("ic_baseline_shoe_default_24"))
 
 class SharedViewModel: ViewModel() {
 
