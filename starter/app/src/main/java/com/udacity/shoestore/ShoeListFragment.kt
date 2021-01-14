56238598d0c4a31129c419145d2a4ac30143be3e
nb26548 <nb26548@novabase.pt>
2021-01-14 22:41:53 +0000

    Added ShoesList from ViewModel to its layout

--- starter/app/src/main/java/com/udacity/shoestore/ShoeListFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/ShoeListFragment.kt
@@ -1,28 +1,61 @@
 package com.udacity.shoestore
 
+import android.app.Activity
 import android.os.Bundle
-import android.util.Log
 import android.view.*
-import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import com.udacity.shoestore.databinding.FragmentShoesListBinding
+import com.udacity.shoestore.databinding.ShoeItemLayoutBinding
+
 
 class ShoeListFragment: Fragment() {
     private val viewModel: SharedViewModel by activityViewModels()
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         //viewModel = ViewModelProvider(requireActivity()).get(SharedViewModel::class.java)
-        val binding: FragmentShoesListBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_shoes_list, container, false)
-        binding.shoeListTitle.text = viewModel.loggedin.value.toString()
+        val binding: FragmentShoesListBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_shoes_list,
+            container,
+            false
+        )
         binding.floatingActionButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_shoeListFragment_to_shoeDetail))
+        viewModel.shoeList.observe(viewLifecycleOwner, Observer { newScore ->
+            newScore.forEach {
+
+                val content = "Shoe Name :${it.name}" +
+                        "\nSize : ${it.size.toString()}" +
+                        "\nCompany : ${it.company} " +
+                        "\nDescription : ${it.description} " +
+                        "\n"
+
+                val itemBinding: ShoeItemLayoutBinding = DataBindingUtil.inflate(
+                    layoutInflater,
+                    com.udacity.shoestore.R.layout.shoe_item_layout,
+                    binding.shoeListView,
+                    false
+                )
+                itemBinding.shoeDetails = content
+                itemBinding.shoeName = it.name
+                val shoeView = itemBinding.root
+
+                binding.shoeListView.addView(shoeView)
+            }
+
+        })
         setHasOptionsMenu(true)
         return binding.root
     }
+
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.toolbar_menu, menu)
