79b63ac407df347fa1b8bc1086f7d89dfa2020ca
catarinaccmateus <catarina_1102@hotmail.com>
2021-01-15 17:58:46 +0000

    Adding more style and removing comments

--- starter/app/src/main/java/com/udacity/shoestore/fragments/ShoeDetailFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/fragments/ShoeDetailFragment.kt
@@ -27,22 +27,7 @@ class ShoeDetailFragment : Fragment() {
         binding.viewModel = viewModel
         binding.setLifecycleOwner(this)
         binding.cancelDetailsButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_shoeDetail_to_shoeListFragment))
-        binding.detailsSaveButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_shoeDetail_to_shoeListFragment))
         return binding.root
     }
 
 }
-
-/*
-        Previous implementation (before two way data binding):
-
-        binding.detailsSaveButton.setOnClickListener {
-            val name = binding.detailsShoeName.text.toString()
-            val company = binding.detailsShoeCompany.text.toString()
-            val description = binding.detailsShoeDescription.text.toString()
-            val size: Double = binding.detailsShoeSize.text.toString().toDoubleOrNull() ?: 0.0
-            val shoe = Shoe(name, size, company, description)
-            viewModel.addShoe(shoe)
-            view?.findNavController()?.navigate(R.id.action_shoeDetail_to_shoeListFragment)
-        }
- */
